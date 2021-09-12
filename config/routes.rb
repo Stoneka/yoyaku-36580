165cd96e5e5f765b46b28803f3b4f41454ab62c6
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-09-12 14:07:22 +0900

    登録商品管理機能、ビュー、manageアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :customers
   root to:'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get 'manage'
+    end
+  end
 end
