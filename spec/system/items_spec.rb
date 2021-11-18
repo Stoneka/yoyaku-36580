de262dfd7dcd175a7bf52d7be9954d012df7c681
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-11-18 23:07:09 +0900

    エンティティの洗い出し

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -2,8 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Items", type: :system do
   before do
-    driven_by(:rack_test)
+    @item = FactoryBot.build(:item)
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+#商品管理機能の結合テストコード記述
+context '商品の新規登録ができるとき' do
+  it '正しい情報を入力すれば商品の新規登録ができる' do
+     #トップページに移動する
+     visit root_path
+     #トップページに新規登録ページに遷移するボタンがあることを確認する
+     #管理者でログインする
+     expect(page).to have_content('新規登録')
+     #商品管理ページへ移動する
+     ##visit new_customer_registration_path
+     #商品情報を入力する
+     #新規登録ボタンを押すとアイテムモデルのカウントが1上がることを確認する
+     ##expect{
+     ##  find('input[name="commit"]').click
+     ##}.to change { Customer.count }.by(1)
+     #トップページへ遷移したことを確認する
+     ##expect(current_path).to eq(root_path)
+     #ログアウトボタンが表示されていることを確認する
+     ##expect(page).to have_content('ログアウト')
+     #新規登録ページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+     ##expect(page).to have_no_content('新規登録')
+     ##expect(page).to have_no_content('ログイン')
+     end
+  end
 end
+#異常系
