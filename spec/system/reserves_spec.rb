246eb85ae849f0c2a93c99887d75a71227454a17
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-12-14 15:30:03 +0900

    予約管理の結合テスト記述

--- spec/system/reserves_spec.rb
+++ spec/system/reserves_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Reserves", type: :system do
     @item = FactoryBot.create(:item)
     @cart = FactoryBot.create(:cart)
     @reserve = FactoryBot.build(:reserve)
+    @user_master = FactoryBot.create(:user_master)
   end
   context '商品の予約ができるとき' do
     it '正しい情報を入力すれば商品が予約できる' do
@@ -75,17 +76,12 @@ RSpec.describe "Reserves", type: :system do
     it 'すでに商品が予約されていれば予約管理ページから予約を確認することができる' do
       #トップページに移動する
       visit root_path
-      #管理者アカウントでログインできていない。一旦ログアウトの操作をしてから再度管理者アカウントでログインする記述が必要
-      #トップページにログアウトボタンが表示されていることを確認する
-      #ログアウトボタンのリンクを確認し、ログアウトボタンをクリックする
-      #ログアウトが完了したことを確認する
-      #トップページに移動する
       #トップページにログインボタンが表示されていることを確認する
       expect(page).to have_content('ログイン')
       #ログインページへ移動する
       visit new_customer_session_path
       #管理者でログインする
-      fill_in 'email', with: 'test@test'
+      fill_in 'email', with: 'user@master.jp'
       fill_in 'password', with: '00000a'
       find('input[name="commit"]').click
       #トップページに遷移したことを確認する
@@ -98,13 +94,13 @@ RSpec.describe "Reserves", type: :system do
       expect(page).to have_content('予約済商品一覧')
     end
   end
-  context '既に予約した商品をカートページで確認することができる' do
-    
-  end
-  context '既に予約した商品をカートページから削除することができる' do
-    
-  end
-  context '既に予約した商品をカートページから購入することができる' do
-    
-  end
+  #context '既に予約した商品をカートページで確認することができる' do
+  #  
+  #end
+  #context '既に予約した商品をカートページから削除することができる' do
+  #  
+  #end
+  #context '既に予約した商品をカートページから購入することができる' do
+  #  
+  #end
 end
